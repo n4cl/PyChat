3becfd8faee2916e1948f4d3a8c56a51ec4bd22d
n4cl <devn4cl@gmail.com>
2023-05-09 20:33:07 +0900

    Add: 画面を追加

---
+++ nginx/static/chat.js
@@ -0,0 +1,21 @@
+'use strict';
+let req_button = document.getElementById("btn_request");
+let inp_textarea = document.getElementById("input-text");
+
+req_button.addEventListener("click", function() {
+  const output = document.getElementById('output');
+  const new_div = document.createElement('div');
+  let text = inp_textarea.value;
+  text = text.trim();
+  if (text === '') {
+    return;
+  }
+  new_div.textContent = text;
+  output.appendChild(new_div);
+});
+
+inp_textarea.addEventListener("keyup", function(event) {
+    if (event.code === "Enter") {
+      req_button.click();
+    }
+});
