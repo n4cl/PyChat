2a68a99f382454866a8a28a4a93b94956d95a3a8
N4CL <devn4cl@gmail.com>
2023-07-02 22:19:46 +0900

    Update: 処理完了時にログを出力するように更新 (#23)

--- app/log.py
+++ app/log.py
@@ -41,14 +41,23 @@ class ContextIncludedRoute(APIRoute):
                     input_field["body"] = (await request.body()).decode("utf-8")
             else:
                 input_field["body"] = None
-            input_field["headers"] = {k.decode("utf-8"): v.decode("utf-8") for (k, v) in request.headers.raw}
+            # input_field["headers"] = {k.decode("utf-8"): v.decode("utf-8") for (k, v) in request.headers.raw}
             input_field["addr"] = request.client.host
             input_field["uri"] = request.url.path
             input_field["method"] = request.method
-            logger.info(json.dumps(input_field))
+            logger.info(json.dumps(input_field, ensure_ascii=False))
 
             # レスポンスオブジェクトの取得
             response: Response = await original_route_handler(request)
+            output_field = {}
+            output_field["I/O"] = "OUT"
+            output_field["timestamp"] = datetime.datetime.fromtimestamp(time.time()).strftime("%Y/%m/%d %H:%M:%S%Z")
+            output_field["request_id"] = request_id
+            output_field["status_code"] = response.status_code
+            # output_field["headers"] = {k: v for (k, v) in response.headers.items()}
+            output_field["body"] = response.body.decode("utf-8")
+            output_field["elapsed_time"] = round(time.time() - begin, 2)
+            logger.info(json.dumps(output_field, ensure_ascii=False))
 
             return response
 
