428bb408f4c5dfe33716b62c88fa0cb105acc40b
n4cl <devn4cl@gmail.com>
2023-11-15 01:08:14 +0900

    Update: OpenAI モジュールのマイグレーション

--- app/chat.py
+++ app/chat.py
@@ -1,16 +1,27 @@
 import openai
+from openai import OpenAI
 from fastapi import status
 
 
 def chat_request(messages: list, model: str):
     try:
-      response = openai.ChatCompletion.create(
-        model=model,
-        temperature=0,
-        messages=messages
-      )
-    except openai.error.InvalidRequestError as e:
+        client = OpenAI()
+        response = client.chat.completions.create(
+            model=model,
+            temperature=0,
+            messages=messages
+        )
+    except openai.BadRequestError as e:
         return str(e), status.HTTP_400_BAD_REQUEST
 
-    content = response["choices"][0]["message"]["content"]
+    content = response.choices[0].message.content
     return content, status.HTTP_200_OK
+
+def get_model_list() -> list:
+    client = OpenAI()
+
+    response = client.models.list()
+    return response.data
+
+if __name__ == "__main__":
+    print(get_model_list())
