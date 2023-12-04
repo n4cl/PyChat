3767e444ea79951d615c4f2a204a60652b104922
N4CL <devn4cl@gmail.com>
2023-12-05 01:16:19 +0900

    Update: より要約されたタイトルが出力されるように更新 (#55)

--- app/chat.py
+++ app/chat.py
@@ -5,13 +5,13 @@ from fastapi import status
 
 def generate_title(message: str):
 
-    prompt = ("質問の内容を要約してタイトルを生成してください。\n"
+    prompt = ("入力された内容を要約してタイトルを生成してください。\n"
               "また生成場合は出力フォーマットにしたがってください。\n\n"
               "## 出力フォーマット\n"
               "文字列\n\n"
               "## 出力例\n"
               "これはタイトル！\n\n"
-              "## 質問内容"
+              "## 入力された内容\n"
               "{message}\n\n"
               "## タイトル\n")
     content = prompt.format(message=message)
